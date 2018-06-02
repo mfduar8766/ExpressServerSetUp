b80c17a54f14b4bea6a9402f1f6ce984cced1a76
Matheus Duarte <30513439+mfduar8766@users.noreply.github.com>
2018-06-02 11:14:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser);
+const bodyParser = require('body-parser');
                            
 //Routes
 const users = require('./routes/api/users');
