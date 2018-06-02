e7a1b334c9da5fd4be4800b2a1cd67b8dcfc71f2
Matheus Duarte <30513439+mfduar8766@users.noreply.github.com>
2018-06-02 14:11:36 -0400

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//@route GET api/test
+//@route GET api/users/test
 //@desc Test route
 //@access Public
 router.get('/test', (req, res) => res.json({msg: "This is a message"}));
